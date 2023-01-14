9e45275651a668914d6ff3b9203dca3ed9da6115
Miguel <miguelrondon92@gmail.com>
2023-01-14 17:25:36 -0500

    completed helper file

--- week_3_data_warehouse/airflow/dags/helper.py
+++ week_3_data_warehouse/airflow/dags/helper.py
@@ -8,12 +8,11 @@ from datetime import datetime
 yellow_trip_2019_01_link= 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2019-10.parquet'
 yellow_trip_2022_05 = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-05.parquet'
 
-
-
 yt_2019_01_file= '~/downloads/yellow_tripdata_2019-01.parquet'
 yt_2022_06_file= '~/downloads/yellow_tripdata_2022-06.parquet'
 yt_2022_05_file= '~/downloads/yellow_tripdata_2022-05.parquet'
-
+gr_2019_01_file= '~/downloads/green_tripdata_2019-01.parquet'
+fhv_2019_01_file= '~/downloads/fhv_tripdata_2019-01.parquet'
 
 yellow_taxi_schema= {
     'VendorID': pa.string(),
@@ -23,9 +22,9 @@ yellow_taxi_schema= {
     'trip_distance': pa.float64(),
     'RatecodeID': pa.string(),
     'store_and_fwd_flag': pa.string(),
-    'PULocationID': pa.int64(),
-    'DOLocationID': pa.int64(),
-    'payment_type': pa.int64(),
+    'PULocationID': pa.string(),
+    'DOLocationID': pa.string(),
+    'payment_type': pa.string(),
     'fare_amount': pa.float64(),
     'extra': pa.float64(),
     'mta_tax': pa.float64(),
@@ -37,33 +36,46 @@ yellow_taxi_schema= {
     'airport_fee': pa.float64(),
     }
 
+yellow_schema = [pa.field(x, y) for x, y in yellow_taxi_schema.items()]
 
-geen_taxi_schema = {
 
+green_taxi_schema = {
+    'VendorID': pa.string(),
+    'lpep_pickup_datetime': pa.timestamp('ns'),
+    'lpep_dropoff_datetime': pa.timestamp('ns'),
+    'store_and_fwd_flag': pa.string(),
+    'RatecodeID': pa.string(),
+    'PULocationID': pa.string(),				
+    'DOLocationID': pa.string(),	
+    'passenger_count': pa.int64(), 
+    'trip_distance': pa.float64(),
+    'fare_amount': pa.float64(),
+    'extra': pa.float64(), 
+    'mta_tax': pa.float64(),	
+    'tip_amount': pa.float64(),		
+    'tolls_amount': pa.float64(),		
+    'ehail_fee': pa.float64(),				
+    'improvement_surcharge': pa.float64(), 
+    'total_amount': pa.float64(), 
+    'payment_type': pa.string(), 
+    'trip_type': pa.string(), 
+    'congestion_surcharge': pa.float64()
 }
 
+green_schema = [pa.field(x, y) for x, y in green_taxi_schema.items()]
 
 fhv_taxi_schema = {
-
+    'dispatching_base_num': pa.string(),
+    'pickup_datetime': pa.timestamp('ns'),
+    'dropOff_datetime': pa.timestamp('ns'), 
+    'PUlocationID': pa.string(),
+    'DOlocationID': pa.string(),
+    'SR_Flag': pa.string(),
+    'Affiliated_base_number': pa.string(),
 }
 
-
-def test_schema(file, taxi):
-    table = pq.read_table(file) 
-    match taxi:
-        case "yellow":
-            fields = [pa.field(x, y) for x, y in yellow_taxi_schema.items()]
-        case "green":
-            fields = [pa.field(x, y) for x, y in green_taxi_schema.items()]
-        case "fhv": 
-            fields = [pa.field(x, y) for x, y in fhv_taxi_schema.items()]
-    new_schema = pa.schema(fields)
-    table = table.cast(new_schema)
-    print(table)
-    #pa.parquet.write_table(table, '~/downloads/modified_file.parquet')
+fhv_schema = [pa.field(x, y) for x, y in fhv_taxi_schema.items()]
 
 
-test_schema(yt_2019_01_file)
-test_schema(yt_2022_06_file)
-test_schema(yt_2022_05_file)
+
 
